5206c64fa5b031422a2bfef565a395453474224d
Kalia Jade Darrow <darrowj08@students.ecu.edu>
2014-09-18 19:35:56 -0400

    Fixed Create-Armor and did stuff.

--- src/com/Plugin/Items/ItemAPI.java
+++ src/com/Plugin/Items/ItemAPI.java
@@ -10,28 +10,34 @@ import org.bukkit.inventory.ItemStack;
 
 public class ItemAPI {
 	
-	private static Material[] Materials = { Material.WOOD_AXE, Material.WOOD_HOE, Material.WOOD_PICKAXE, Material.WOOD_SPADE, Material.WOOD_SWORD, Material.STONE_AXE, Material.STONE_HOE, Material.STONE_PICKAXE, Material.STONE_SPADE, Material.STONE_SWORD, Material.IRON_AXE, Material.IRON_HOE, Material.IRON_PICKAXE, Material.IRON_SPADE, Material.IRON_SWORD, Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SPADE, Material.DIAMOND_SWORD, Material.GOLD_AXE, Material.GOLD_HOE, Material.GOLD_PICKAXE, Material.GOLD_SPADE, Material.GOLD_SWORD, Material.LEATHER_HELMET,Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS,Material.LEATHER_CHESTPLATE,Material.IRON_HELMET, Material.IRON_LEGGINGS,Material.IRON_BOOTS,Material.IRON_CHESTPLATE,Material.GOLD_HELMET,Material.GOLD_LEGGINGS, Material.GOLD_BOOTS, Material.GOLD_CHESTPLATE,Material.DIAMOND_HELMET,Material.DIAMOND_LEGGINGS,Material.DIAMOND_BOOTS, Material.DIAMOND_CHESTPLATE,Material.CHAINMAIL_BOOTS,Material.CHAINMAIL_CHESTPLATE,Material.CHAINMAIL_HELMET,Material.CHAINMAIL_LEGGINGS };
-	private static Material[][] Armor = {{Material.LEATHER_HELMET, Material.IRON_HELMET, Material.GOLD_HELMET, Material.DIAMOND_HELMET,Material.CHAINMAIL_HELMET},{Material.LEATHER_CHESTPLATE, Material.IRON_CHESTPLATE, Material.GOLD_CHESTPLATE, Material.DIAMOND_CHESTPLATE,Material.CHAINMAIL_CHESTPLATE},{Material.LEATHER_LEGGINGS, Material.IRON_LEGGINGS, Material.GOLD_LEGGINGS, Material.DIAMOND_LEGGINGS,Material.CHAINMAIL_LEGGINGS},{Material.LEATHER_BOOTS, Material.IRON_BOOTS, Material.GOLD_BOOTS, Material.DIAMOND_BOOTS,Material.CHAINMAIL_BOOTS}};
+	private static Material[] Materials = {  Material.BOW, Material.WOOD_AXE, Material.WOOD_HOE, Material.WOOD_PICKAXE, Material.WOOD_SPADE, Material.WOOD_SWORD, Material.STONE_AXE, Material.STONE_HOE, Material.STONE_PICKAXE, Material.STONE_SPADE, Material.STONE_SWORD, Material.IRON_AXE, Material.IRON_HOE, Material.IRON_PICKAXE, Material.IRON_SPADE, Material.IRON_SWORD, Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SPADE, Material.DIAMOND_SWORD, Material.GOLD_AXE, Material.GOLD_HOE, Material.GOLD_PICKAXE, Material.GOLD_SPADE, Material.GOLD_SWORD, Material.LEATHER_HELMET,Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS,Material.LEATHER_CHESTPLATE,Material.IRON_HELMET, Material.IRON_LEGGINGS,Material.IRON_BOOTS,Material.IRON_CHESTPLATE,Material.GOLD_HELMET,Material.GOLD_LEGGINGS, Material.GOLD_BOOTS, Material.GOLD_CHESTPLATE,Material.DIAMOND_HELMET,Material.DIAMOND_LEGGINGS,Material.DIAMOND_BOOTS, Material.DIAMOND_CHESTPLATE,Material.CHAINMAIL_BOOTS,Material.CHAINMAIL_CHESTPLATE,Material.CHAINMAIL_HELMET,Material.CHAINMAIL_LEGGINGS };
+	private static Material[][] Armor = {{Material.LEATHER_HELMET, Material.IRON_HELMET, Material.GOLD_HELMET, Material.DIAMOND_HELMET,Material.CHAINMAIL_HELMET},{Material.LEATHER_CHESTPLATE, Material.IRON_CHESTPLATE, Material.GOLD_CHESTPLATE, Material.DIAMOND_CHESTPLATE,Material.CHAINMAIL_CHESTPLATE},{Material.LEATHER_LEGGINGS, Material.IRON_LEGGINGS, Material.GOLD_LEGGINGS, Material.DIAMOND_LEGGINGS,Material.CHAINMAIL_LEGGINGS},{Material.LEATHER_BOOTS, Material.IRON_BOOTS, Material.GOLD_BOOTS, Material.DIAMOND_BOOTS,Material.CHAINMAIL_BOOTS}, { Material.BOW, Material.WOOD_AXE, Material.WOOD_HOE, Material.WOOD_PICKAXE, Material.WOOD_SPADE, Material.WOOD_SWORD, Material.STONE_AXE, Material.STONE_HOE, Material.STONE_PICKAXE, Material.STONE_SPADE, Material.STONE_SWORD, Material.IRON_AXE, Material.IRON_HOE, Material.IRON_PICKAXE, Material.IRON_SPADE, Material.IRON_SWORD, Material.DIAMOND_AXE, Material.DIAMOND_HOE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SPADE, Material.DIAMOND_SWORD, Material.GOLD_AXE, Material.GOLD_HOE, Material.GOLD_PICKAXE, Material.GOLD_SPADE, Material.GOLD_SWORD}};
 	/**
 	 * Create Armor                         
 	 * <p>
 	 * Creates an armor item based on a passed in parameter, with random enchants and type.
 	 * 
-	 * @param  type 0 = Helmet, 1 = Chestplate, 2 = Leggings, 3 = Boots
+	 * @param  type 0 = Helmet, 1 = Chestplate, 2 = Leggings, 3 = Boots, 4 = Held weapons
 	 * @return The generated Item
 	 */
 	public static ItemStack createArmor(int type) {
-		Material Material = Materials[Plugin.Random.nextInt(Armor[type].length)];
+		Material Material = Armor[type][Plugin.Random.nextInt(Armor[type].length)];
 		ItemStack Item = new ItemStack(Material);
 		return diablofy(Item);
 	}
+	/**
+	 * Diablofy
+	 * <p>
+	 * Takes an item and adds random ass enchantments to it.
+	 * @param Item
+	 * @return Funky Item
+	 */
 	public static ItemStack diablofy(ItemStack Item) {
 
 		Item.setDurability((short)Plugin.Random.nextInt(120));
-		
-		
 		////////////////Set max enchants to this value///////////////
 		int maxEnchants = 6;
+		/////////////////
 		int[] enchantsTable = {0, 1, 2, 3,4 ,5, 6, 7, 16, 17,18, 19, 20, 21,32,33,34,35,48,49,50,51};
 		int hasEnchants = Plugin.Random.nextInt(maxEnchants);
 		for(int i = 0; i<hasEnchants;i++)
