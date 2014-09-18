315c35c4ce85af9a21e37a0c5a18ef73a4406f4e
Kalia Jade <unarmed1618@users.noreply.github.com>
2014-09-17 20:23:48 -0400

    Updating the material and area checker

--- src/com/Plugin/Events/ChunkEvents.java
+++ src/com/Plugin/Events/ChunkEvents.java
@@ -2,6 +2,7 @@ package com.Plugin.Events;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
@@ -16,7 +17,42 @@ import com.Plugin.Generation.Schematic;
 import com.Plugin.Main.Plugin;
 
 public class ChunkEvents implements Listener {
-	
+	private boolean isBlockValid(World world, int x, int i, int z) {
+		//Add a config element for size of spawn flat
+		
+		int m = 5; // Step size. Diameter is 4n.
+		
+		//Valid materials are set here.
+		
+		ArrayList<Material> valid = new ArrayList<Material>();
+		valid.add(Material.DIRT);
+		valid.add(Material.SAND);
+		valid.add(Material.GRASS);
+		valid.add(Material.STONE);
+		
+		
+		// Logic below. Don't change this unless you want to break things.
+		if(valid.contains(world.getBlockAt(x, i, z))
+				&&valid.contains(world.getBlockAt(x+2*m, i, z+2*m))
+				&&valid.contains(world.getBlockAt(x-2*m, i, z-2*m))
+				&&valid.contains(world.getBlockAt(x+2*m, i, z))
+				&&valid.contains(world.getBlockAt(x, i, z+2*m))
+				&&valid.contains(world.getBlockAt(x-2*m, i, z))
+				&&valid.contains(world.getBlockAt(x, i, z-2*m))
+				&&valid.contains(world.getBlockAt(x+2*m, i, z-2*m))
+				&&valid.contains(world.getBlockAt(x-2*m, i, z+2*m))
+				&&valid.contains(world.getBlockAt(x+m, i, z))
+				&&valid.contains(world.getBlockAt(x, i, z+m))
+				&&valid.contains(world.getBlockAt(x-m, i, z))
+				&&valid.contains(world.getBlockAt(x, i, z-m))
+				&&valid.contains(world.getBlockAt(x+m, i, z+m))
+				&&valid.contains(world.getBlockAt(x-m, i, z-m))
+				&&valid.contains(world.getBlockAt(x+m, i, z-m))
+				&&valid.contains(world.getBlockAt(x-m, i, z+m)))
+		return true;
+		else 
+			return false;
+	}
 	@EventHandler
 	public void onChunkLoaded(ChunkPopulateEvent event) {
 		if (Plugin.Random.nextInt(100) <= Plugin.Settings.getInt("Dungeons.Chance")) {
@@ -28,7 +64,9 @@ public class ChunkEvents implements Listener {
 			int z=16*Chunk.getZ()*Plugin.Random.nextInt(15);
 			
 			for (int i=World.getMaxHeight()-1; i>0; i--) {
-				if (World.getBlockAt(x, i, z).getType() != Material.AIR) {
+				//if (World.getBlockAt(x, i, z).getType() != Material.AIR) 
+				if(isBlockValid(World,x, i, z))
+				{
 					y = i;
 					break;
 				}
