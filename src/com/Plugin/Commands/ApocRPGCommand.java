def7e3bfbc569faaee5ddafbd65208c65429e266
WIX0A <simonborg9223@yahoo.com>
2014-09-21 10:32:06 -0400

    Fixed /generateuseful

--- src/com/Plugin/Commands/ApocRPGCommand.java
+++ src/com/Plugin/Commands/ApocRPGCommand.java
@@ -7,29 +7,34 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import com.Plugin.Items.ItemAPI;
+import com.Plugin.Main.Plugin;
 
-public class ApocRPGCommand implements CommandExecutor{
+public class ApocRPGCommand implements CommandExecutor {
 
 	
 	@Override
 	public boolean onCommand(CommandSender Sender, Command Command, String arg2, String[] args) {
 		Player Player = (Player) Sender;
-		if(Command.getLabel().equals("generateitem")) {
+		if (Command.getLabel().equals("generateitem")) {
 			ItemStack item = ItemAPI.createItem();
-			Player.getInventory().addItem(item);
+			if (args.length==0) {
+				Player.getInventory().addItem(item);
+			} else {
+				Player TargetPlayer = Plugin.Plugin.getServer().getPlayer(args[0]);
+				TargetPlayer.getInventory().addItem(item);
+			}
 			return true;
-		} else if(Command.getLabel().equals("generateuseful")) {
+		} else if (Command.getLabel().equals("generateuseful")) {
 			ItemStack item;
 			if(args.length==0) {
 				item = ItemAPI.generateUsefulItem(0);
-				return true;
 			} else {
 				item = ItemAPI.generateUsefulItem(Integer.parseInt(args[0]));
-				Player.getInventory().addItem(item);
-				return true;
 			}
+			Player.getInventory().addItem(item);
+			return true;
 		} else {
-			System.out.println("Something went wrong");
+			System.out.println("Unkown command sent.");
 			return false;
 		}
 	}
