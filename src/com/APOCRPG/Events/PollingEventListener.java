50a6c86d7da5669851e4c2706ca46357229fd5ea
Troy "Magicslayer" Adamczyk <adamczykcgs@gmail.com>
2016-03-02 17:49:48 -0500

    Error free atm.
    Ok, I managed to fix the errors that had occured when I added the onDamage and moved some of the variables around. Next is the entity count to finish the courage gem and remove the potion effects in the gems that deal with player damage.

--- src/com/APOCRPG/Events/PollingEventListener.java
+++ src/com/APOCRPG/Events/PollingEventListener.java
@@ -22,6 +22,17 @@ import com.APOCRPG.API.EffectAPI;
 public final class PollingEventListener implements Listener {	
 	
 	double damage = 0;
+	double resist = 0;
+	int entcount = 0;
+	
+	//Direct damage handler
+	@EventHandler
+    public void onDamage(EntityDamageEvent event2) {
+        damage = event2.getDamage();
+        if (event2.getEntity() instanceof Player) {
+            Player player = (Player) event2.getEntity();
+        }
+	}
 	//This array needs to point to a .yml for adding modpack hostiles
 	private static ArrayList<EntityType> Hostiles = new ArrayList<EntityType>();
 
@@ -62,13 +73,6 @@ public final class PollingEventListener implements Listener {
 			if (a != null && a.getItemMeta() != null) {
 				stuff.add(a);// Keep only the non null armor objects
 			}
-		}
-		@EventHandler
-	    public void onDamage(EntityDamageEvent event2) {
-	        damage = event2.getDamage();
-	        if (event2.getEntity() instanceof Player) {
-	            Player player = (Player) event2.getEntity();
-	        }
 		}
 		
 		if (!stuff.isEmpty()) {
@@ -127,8 +131,9 @@ public final class PollingEventListener implements Listener {
 					case "Courage":
 						for (Entity e : p.getNearbyEntities(effect.getValue() + 4.0, effect.getValue() + 4.0, effect.getValue() + 4.0))
 							if (!e.getType().equals(EntityType.PLAYER))	{
-								int entcount = entcount + 2; //2 is filler
-								damage = damage - damage * .025;  //damage is defined in the event call onDamage but doesn't like the call down here.
+								entcount = entcount + 2; //2 is filler
+								resist = resist + entcount;
+								damage = damage - damage * resist;  //damage is defined in the event call onDamage but doesn't like the call down here.
 																//also the resistence amount .025 will be moved to a variable so levels can be added to it
 			
 							}					
