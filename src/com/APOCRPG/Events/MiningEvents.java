f531ad45fe8e00264e98863e35d93ac0bb2e153d
Troy "Magicslayer" Adamczyk <adamczykcgs@gmail.com>
2016-03-25 03:23:47 -0400

    added breaking

--- src/com/APOCRPG/Events/MiningEvents.java
+++ src/com/APOCRPG/Events/MiningEvents.java
@@ -1,6 +1,20 @@
 package com.APOCRPG.Events;
 
-public class MiningEvents {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+import com.APOCRPG.API.EffectAPI;
+
+public class MiningEvents implements Listener {
+	@EventHandler
+	public void onBlockBreak(BlockDamageEvent eve) {
+		if (EffectAPI.getEffectsFromItem(eve.getPlayer().getItemInHand()).containsKey("Breaking")) {
+			eve.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, EffectAPI.getEffectsFromItem(eve.getPlayer().getItemInHand()).get("Breaking"), 20));
+		}
+
+	}
+		
 }
-
